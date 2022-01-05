931ec99d74dd40544e790d6715b8cdb72b313840
alexandreroget <alexandre.roget@inria.fr>
2022-01-05 14:46:20 +0100

    Correction de examples/CMakeLists.txt

---
+++ build/examples/CMakeFiles/GRIN2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GRIN2.dir/GRIN2.cpp.o"
+  "GRIN2"
+  "GRIN2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GRIN2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/matio/CMakeFiles/mainMatio.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainMatio.dir/mainMatio.cpp.o"
+  "mainMatio"
+  "mainMatio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainMatio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
